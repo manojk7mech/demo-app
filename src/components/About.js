87ef04859aa51588122aed38960833febb618d6f
Manoj Kumar <manojk7mech@gmail.com>
2021-09-25 10:25:30 +0530

    some changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ const About = () => {
     return (
         <div className="dark:bg-gray-700 pb-4">
             <h1 className="text-center pt-6 pb-4 text-2xl font-bold uppercase text-blue-500 dark:text-green-500">It is actually 'about me' (For now atleast)!</h1>
-            <h3 className="text-center text-lg text-blue-400 dark:text-green-600 font-semibold block pb-3">
-                Hi, I am <span className="text-pink-400 font-bold">Manoj Kumar</span><br />Mechanical Engineer by Degree...<br /><code>WebDev</code> by Passion...</h3>
+            <h3 className="text-center text-lg text-gray-900 dark:text-gray-300 font-semibold block pb-3">
+                Hi, I am <span className="text-black dark:text-white font-bold">Manoj Kumar</span><br />Mechanical Engineer by Degree...<br /><code>WebDev</code> by Passion...</h3>
         </div>
     );
 }
